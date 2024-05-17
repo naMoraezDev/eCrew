ad703315e7d97be9578642ac59d35462b469f8a6
Gabriel Moraes <gabrielmoraes@lance.com.br>
2024-05-16 22:35:48 -0300

    fix: featured posts section title gap

--- src/ui/featured-posts/view.tsx
+++ src/ui/featured-posts/view.tsx
@@ -22,7 +22,7 @@ export function FeaturedPostsView({ postList }: FeaturedPostsProps) {
           className="h-[320px] w-full object-cover absolute top-0 left-0 blur-md"
         />
       ))}
-      <div className="w-full h-full absolute top-0 left-0 flex flex-col gap-2 justify-center p-10 z-10">
+      <div className="w-full h-full absolute top-0 left-0 flex flex-col gap-3 justify-center p-10 z-10">
         <h2 className="text-md font-kanit font-bold">Em destaque</h2>
         <PostCard
           variant="outlined"
