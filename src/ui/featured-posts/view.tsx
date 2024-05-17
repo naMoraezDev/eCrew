0549bde9b576feacd74d3b64b576039e54f5e48d
Gabriel Moraes <gabrielmoraes@lance.com.br>
2024-05-16 21:18:56 -0300

    style: featured posts component

--- src/ui/featured-posts/view.tsx
+++ src/ui/featured-posts/view.tsx
@@ -22,10 +22,15 @@ export function FeaturedPostsView({ postList }: FeaturedPostsProps) {
           className="h-[320px] w-full object-cover absolute top-0 left-0 blur-md"
         />
       ))}
-      <div className="w-full h-full absolute top-0 left-0 flex flex-col gap-4 justify-end p-10 z-10">
-        <PostCard orientation="horizontal" post={postList.posts[0]} />
+      <div className="w-full h-full absolute top-0 left-0 flex flex-col gap-2 justify-center p-10 z-10">
+        <h2 className="text-md font-kanit font-bold">Em destaque</h2>
+        <PostCard
+          variant="outlined"
+          orientation="horizontal"
+          post={postList.posts[0]}
+        />
       </div>
-      <div className="w-full h-full absolute top-0 left-0 bg-gradient-to-t from-gray-950 to-transparent" />
+      <div className="w-full h-full absolute top-0 left-0 bg-gradient-to-t from-gray-950 to-[rgba(0,0,0,0.2)]" />
     </section>
   );
 }
