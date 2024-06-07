a3f413150e5c2ef87b0e7583a77dd63e571057f9
Gabriel Moraes <gabrielmoraes@lance.com.br>
2024-06-07 17:50:03 -0300

    feat: creates redirect for home path

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  redirects: async () => {
+    return [
+      {
+        source: "/",
+        permanent: false,
+        destination: "/noticias",
+      },
+    ];
+  },
   images: {
     domains: [
       "i.ibb.co",
