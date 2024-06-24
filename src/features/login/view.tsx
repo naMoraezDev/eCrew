941048e0077b4ba4441a5cf8c016582336a41e44
Gabriel Moraes <gabrielmoraes@lance.com.br>
2024-06-24 15:46:59 -0300

    refactor: moves social login into a separated component

--- src/features/login/view.tsx
+++ src/features/login/view.tsx
@@ -1,35 +1,33 @@
 import Link from "next/link";
 import Image from "next/image";
 import { LoginProps } from "./types";
-import { FcGoogle } from "react-icons/fc";
-import { FaTwitch } from "react-icons/fa";
-import { FaFacebook } from "react-icons/fa";
 import { Button } from "@/components/ui/button";
 import { CustomInput } from "@/ui/custom-input";
+import { SocialLogin } from "@/ui/social-login";
 import ePostsLogo from "@/assets/images/e_posts_logo.svg";
 
 export function LoginView({}: LoginProps) {
   return (
     <section className="w-full h-screen flex justify-center items-center bg-gradient-to-bl from-zinc-950 to-violet-950">
-      <div className="bg-zinc-900 gap-3 rounded-lg p-6 w-full max-w-md">
+      <div className="bg-zinc-900 gap-6 rounded-lg p-6 w-full max-w-md flex flex-col">
+        <div className="flex items-center gap-1 relative w-full justify-center">
+          <Image
+            priority
+            width={24}
+            height={24}
+            src={ePostsLogo}
+            alt="ePosts logo"
+          />
+          <span className="font-kanit text-xl">ePosts</span>
+          <Link
+            href="/noticias"
+            className="w-full h-full absolute top-0 left-0"
+          />
+        </div>
+        <span className="text-sm font-kanit w-full flex justify-center">
+          Entre com sua conta ePosts
+        </span>
         <form className="flex flex-col gap-6">
-          <div className="flex items-center gap-1 relative w-full justify-center">
-            <Image
-              priority
-              width={24}
-              height={24}
-              src={ePostsLogo}
-              alt="ePosts logo"
-            />
-            <span className="font-kanit text-xl">ePosts</span>
-            <Link
-              href="/noticias"
-              className="w-full h-full absolute top-0 left-0"
-            />
-          </div>
-          <span className="text-sm font-kanit w-full flex justify-center">
-            Entre com sua conta ePosts
-          </span>
           <div className="w-full flex flex-col gap-3">
             <CustomInput
               name="email"
@@ -62,30 +60,8 @@ export function LoginView({}: LoginProps) {
               Cadastre-se no nosso site!
             </span>
           </span>
-          <div className="w-full flex flex-col gap-3">
-            <button
-              type="button"
-              className="flex items-center justify-center gap-2 text-sm rounded-lg bg-zinc-800 p-2"
-            >
-              <FcGoogle />
-              Entre com o Google
-            </button>
-            <button
-              type="button"
-              className="flex items-center justify-center gap-2 text-sm rounded-lg bg-zinc-800 p-2"
-            >
-              <FaFacebook className="text-blue-500" />
-              Entre com o Facebook
-            </button>
-            <button
-              type="button"
-              className="flex items-center justify-center gap-2 text-sm rounded-lg bg-zinc-800 p-2"
-            >
-              <FaTwitch className="text-violet-500" />
-              Entre com a Twitch
-            </button>
-          </div>
         </form>
+        <SocialLogin />
       </div>
     </section>
   );
---
+++ src/ui/social-login/view.tsx
@@ -0,0 +1,31 @@
+import { FcGoogle } from "react-icons/fc";
+import { FaTwitch } from "react-icons/fa";
+import { FaFacebook } from "react-icons/fa";
+
+export function SocialLoginView() {
+  return (
+    <div className="w-full flex flex-col gap-3">
+      <button
+        type="button"
+        className="flex items-center justify-center gap-2 text-sm rounded-lg bg-zinc-800 p-2"
+      >
+        <FcGoogle />
+        Entre com o Google
+      </button>
+      <button
+        type="button"
+        className="flex items-center justify-center gap-2 text-sm rounded-lg bg-zinc-800 p-2"
+      >
+        <FaFacebook className="text-blue-500" />
+        Entre com o Facebook
+      </button>
+      <button
+        type="button"
+        className="flex items-center justify-center gap-2 text-sm rounded-lg bg-zinc-800 p-2"
+      >
+        <FaTwitch className="text-violet-500" />
+        Entre com a Twitch
+      </button>
+    </div>
+  );
+}
