57fbc078b4417d8f767472722436929b7b4a4c0e
Gabriel Moraes <gabrielmoraes@lance.com.br>
2024-06-24 16:54:24 -0300

    style: removes navbar borders

--- src/ui/layout-wrapper/navbar/view.tsx
+++ src/ui/layout-wrapper/navbar/view.tsx
@@ -1,6 +1,6 @@
 export function NavbarView() {
   return (
-    <nav className="w-full h-8 bg-gradient-to-r from-zinc-950 to-zinc-900 border-t border-b border-zinc-800 overflow-hidden">
+    <nav className="w-full h-8 bg-gradient-to-r from-zinc-950 to-zinc-900 overflow-hidden">
       <div className="w-full max-w-[1270px] mx-auto px-4 py-2 flex items-center gap-6 font-bold text-xs"></div>
     </nav>
   );
