c70e40a191edb876bff0fce0de4d1b17dcb3d31c
Gabriel Moraes <gabrielmoraes@lance.com.br>
2024-06-28 10:22:40 -0300

    fix: general fixes

--- src/infrastructure/adapters/implementation/fetch-http-client.adapter.ts
+++ src/infrastructure/adapters/implementation/fetch-http-client.adapter.ts
@@ -3,9 +3,9 @@ import { HttpClient, HttpRequest } from "../factories/http-client.factory";
 export class FetchHttpClientAdapter implements HttpClient {
   async request({ input, init }: HttpRequest) {
     const response = await fetch(`${input}`, {
+      body: init?.body,
       method: init?.method,
       headers: init?.headers,
-      body: JSON.stringify(init?.body),
     });
     return await response.json();
   }
