3b32e730398d68ac79136fa2b040ea9c83868c02
Gabriel Moraes <gabrielmoraes@lance.com.br>
2024-06-29 02:17:16 -0300

    fix: general fixes

--- src/ui/user-menu/_io.ts
+++ src/ui/user-menu/_io.ts
@@ -1,8 +1,11 @@
+import { useRouter } from "next/navigation";
 import { firebaseClient } from "@/services/firebase/firebase-client";
 
 export function useUserMenu() {
+  const router = useRouter();
   async function signOut() {
     await firebaseClient.auth().signOut();
+    router.refresh();
   }
 
   return { signOut };
