8452f3928d513d20ac4a35e58600c8b62cd2a33a
Gabriel Moraes <gabrielmoraes@lance.com.br>
2024-07-09 12:42:49 -0300

    refactor: middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,7 +4,6 @@ export const middleware = (request: NextRequest) => {
   const { device } = userAgent(request);
   const url = request.nextUrl.clone();
   const isDesktop = device.type !== "mobile";
-
   if (isDesktop) {
     url.pathname = `/desktop${request.nextUrl.pathname}`;
     return NextResponse.rewrite(url);
