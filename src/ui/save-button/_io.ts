ed869f11c47fb421a5276e1eb7121c009393694d
Gabriel Moraes <gabrielmoraes@lance.com.br>
2024-07-14 14:16:51 -0300

    feat: threat sign out user save button

--- src/ui/save-button/_io.ts
+++ src/ui/save-button/_io.ts
@@ -1,10 +1,13 @@
+import { toast } from "sonner";
 import { useAuth } from "@/contexts/auth";
 import { SaveButtonProps } from "./types";
+import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 import { EcrewApiService } from "@/services/ecrew-api.service";
 import { httpClientFactory } from "@/infrastructure/adapters/factories/http-client.factory";
 
 export function useSaveButton({ postSlug }: SaveButtonProps) {
+  const router = useRouter();
   const { user, preferences } = useAuth();
   const [isSaved, setIsSaved] = useState(
     Boolean(preferences?.saved_posts?.includes(postSlug))
@@ -16,6 +19,10 @@ export function useSaveButton({ postSlug }: SaveButtonProps) {
   }, [preferences, postSlug]);
 
   async function handleSave() {
+    if (!user) {
+      toast.error("Você precisa estar logado para salvar posts.");
+      router.push("/login");
+    }
     if (!isSaved) {
       setIsSaved(true);
       await new EcrewApiService(httpClientFactory()).pushToSavedPosts({
