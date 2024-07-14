c017a6333a4251b490bee16a8975a1a97033a33a
Gabriel Moraes <gabrielmoraes@lance.com.br>
2024-07-14 13:11:20 -0300

    fix: threat social login new users preferences creation

--- src/ui/social-login/_io.ts
+++ src/ui/social-login/_io.ts
@@ -1,8 +1,10 @@
 import { toast } from "sonner";
 import { useRouter } from "next/navigation";
 import { useLoading } from "@/contexts/loading";
+import { EcrewApiService } from "@/services/ecrew-api.service";
 import { getFirebaseErrorMessage } from "@/shared/utils/firebase";
 import { firebaseClient } from "@/services/firebase/firebase-client";
+import { httpClientFactory } from "@/infrastructure/adapters/factories/http-client.factory";
 
 export function useSocialLogin() {
   const router = useRouter();
@@ -12,7 +14,15 @@ export function useSocialLogin() {
     setIsLoading(true);
     try {
       const provider = new firebaseClient.auth.GoogleAuthProvider();
-      await firebaseClient.auth().signInWithPopup(provider);
+      const user = await firebaseClient.auth().signInWithPopup(provider);
+      if (user.additionalUserInfo?.isNewUser) {
+        await new EcrewApiService(httpClientFactory()).createUserPreferences({
+          authorization: (await user.user?.getIdToken()) || "",
+          userPreferences: {
+            newsletter: false,
+          },
+        });
+      }
       setIsLoading(false);
       router.push("/noticias");
     } catch (error: any) {
