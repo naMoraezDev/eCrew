abbbf18610ebb67cdd0efb98942fb04fd4995e26
Gabriel Moraes <gabrielmoraes@lance.com.br>
2024-11-09 02:28:45 -0300

    chore: set revalidate time to one hour

--- src/shared/constants.ts
+++ src/shared/constants.ts
@@ -1,4 +1,4 @@
-export const REVALIDATE_TIME = 60 * 10; // 10 minutes
+export const REVALIDATE_TIME = 60 * 60; // 1 hour
 
 export const FIREBASE_ERROR_MESSAGES = {
   "auth/app-deleted": "O banco de dados não foi localizado.",
